764ce131180d2008bd4999f1f1df16437bb32d57
aksami7860 <ak_sami@live.com>
2026-06-07 21:00:17 +0500

    Update app.py

--- app.py
+++ app.py
@@ -5,7 +5,7 @@ from datetime import date
 st.set_page_config(
     page_title="Meezan Bank - Branch Daily Performance Report", 
     layout="wide",
-    "https://www.meezanbank.com/wp-content/uploads/2020/06/logo.png", width=120
+    page_icon="https://www.meezanbank.com/wp-content/uploads/2020/06/logo.png"
 )
 
 # Meezan Bank Brand Colors
